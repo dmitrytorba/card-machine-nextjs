09f49efe4cf36c1fba78d74e2438f59d6c8169e5
Dmitry Torba <dmitrytorba@gmail.com>
2022-08-26 15:51:22 -0700

    feat(nav) nav with page name

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Nav from '../components/nav';
 export default function Home() {
   return (
     <>
-      <Nav pageName="Habits" />
+      <Nav pageName="Card Machine" />
       <div className="container">
         <main>
           <h1 className="title">HAHA</h1>
