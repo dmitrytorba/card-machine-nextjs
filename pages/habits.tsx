e532bd8d87bf861175ea6549868f20a87cc32dde
Dmitry Torba <dmitrytorba@gmail.com>
2022-08-26 19:24:44 -0700

    feat(habits) basic list

--- pages/habits.tsx
+++ pages/habits.tsx
@@ -1,14 +1,29 @@
 import Nav from '../components/nav';
 
-export default function Home() {
+type Habit = {
+  label: string;
+};
+
+export default function Habits({ habits }) {
+  const renderedHabits = habits.map((habit: Habit) => {
+    return <h1 className="title">{habit.label}</h1>;
+  });
   return (
     <>
       <Nav pageName="Habits" />
       <div className="container">
-        <main>
-          <h1 className="title">HAHA</h1>
-        </main>
+        <main>{renderedHabits}</main>
       </div>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const habits = [{ label: 'one' }, { label: 'two' }];
+
+  return {
+    props: {
+      habits,
+    },
+  };
 }
